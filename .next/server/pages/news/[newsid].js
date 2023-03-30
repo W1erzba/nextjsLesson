91734d653d2fc5283b165c07dbbff96916e2f8ef
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-30 18:50:31 +0200

    added a dynamic routepage with extractic dynamic parametere value - useRouter hook from next/router

---
+++ .next/server/pages/news/[newsid].js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/news/[newsid]";
+exports.ids = ["pages/news/[newsid]"];
+exports.modules = {
+
+/***/ "./pages/news/[newsid].js":
+/*!********************************!*\
+  !*** ./pages/news/[newsid].js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/router */ \"next/router\");\n/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_1__);\n\n\n// our-domain.com/news/something-important\nfunction DetailPage() {\n    const router = (0,next_router__WEBPACK_IMPORTED_MODULE_1__.useRouter)();\n    const newsId = router.query.newsId;\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"The Detail Page\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\wierz\\\\Documents\\\\GitHub\\\\NextJS\\\\deep dive in to NextJS\\\\pages\\\\news\\\\[newsid].js\",\n        lineNumber: 10,\n        columnNumber: 9\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (DetailPage);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9uZXdzL1tuZXdzaWRdLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUF3QztBQUV4QywwQ0FBMEM7QUFFMUMsU0FBU0MsYUFBYTtJQUNyQixNQUFNQyxTQUFTRixzREFBU0E7SUFFeEIsTUFBTUcsU0FBU0QsT0FBT0UsS0FBSyxDQUFDRCxNQUFNO0lBRWxDLHFCQUFPLDhEQUFDRTtrQkFBRzs7Ozs7O0FBQ1o7QUFFQSxpRUFBZUosVUFBVUEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL25leHRqcy1jb3Vyc2UvLi9wYWdlcy9uZXdzL1tuZXdzaWRdLmpzPzcyYWMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgdXNlUm91dGVyIH0gZnJvbSAnbmV4dC9yb3V0ZXInO1xyXG5cclxuLy8gb3VyLWRvbWFpbi5jb20vbmV3cy9zb21ldGhpbmctaW1wb3J0YW50XHJcblxyXG5mdW5jdGlvbiBEZXRhaWxQYWdlKCkge1xyXG5cdGNvbnN0IHJvdXRlciA9IHVzZVJvdXRlcigpO1xyXG5cclxuXHRjb25zdCBuZXdzSWQgPSByb3V0ZXIucXVlcnkubmV3c0lkO1xyXG5cclxuXHRyZXR1cm4gPGgxPlRoZSBEZXRhaWwgUGFnZTwvaDE+O1xyXG59XHJcblxyXG5leHBvcnQgZGVmYXVsdCBEZXRhaWxQYWdlO1xyXG4iXSwibmFtZXMiOlsidXNlUm91dGVyIiwiRGV0YWlsUGFnZSIsInJvdXRlciIsIm5ld3NJZCIsInF1ZXJ5IiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/news/[newsid].js\n");
+
+/***/ }),
+
+/***/ "next/router":
+/*!******************************!*\
+  !*** external "next/router" ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = require("next/router");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/news/[newsid].js"));
+module.exports = __webpack_exports__;
+
+})();
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e61151f37c2414bc"; }
+/******/ 		__webpack_require__.h = function() { return "83d4b3ae8c071eb2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
